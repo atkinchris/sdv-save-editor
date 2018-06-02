7fc041f0e8e5aa20ba35d9ccfbc092b86788d673
Chris Atkin <atkinchris@gmail.com>
2018-06-02 19:56:29 +0100

    Remove all trees from farm

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 const fs = require('fs-extra')
 const { DOMParser, XMLSerializer } = require('xmldom')
+const { NodeList } = require('xmldom/dom')
+
+NodeList.prototype.find = Array.prototype.find
+NodeList.prototype.forEach = Array.prototype.forEach
+NodeList.prototype.filter = Array.prototype.filter
 
 const save = 'saves/Chris_187014416'
+
+const findLocation = (doc, name) => doc
+  .getElementsByTagName('SaveGame')[0]
+  .getElementsByTagName('locations')[0]
+  .getElementsByTagName('GameLocation')
+  .find(tag => (
+    tag.getElementsByTagName('name').find(({ textContent }) => textContent === name)
+  ))
 
 const processSave = async () => {
   const xml = await fs.readFile(save, 'utf8')
   const parser = new DOMParser()
   const doc = parser.parseFromString(xml)
 
-  const locations = doc
-    .getElementsByTagName('SaveGame')[0]
-    .getElementsByTagName('locations')[0]
-    .getElementsByTagName('GameLocation')
+  const terrainFeatures = findLocation(doc, 'Farm').getElementsByTagName('terrainFeatures')[0]
 
-  const farm = Array.prototype.find.call(locations, tag => tag.getAttribute('xsi:type') === 'Farm')
+  terrainFeatures
+    .getElementsByTagName('item')
+    .forEach((feature) => {
+      const type = feature.getElementsByTagName('TerrainFeature')[0].getAttribute('xsi:type')
+
+      if (type === 'Tree') {
+        terrainFeatures.removeChild(feature)
+      }
+    })
 
   const xmlSerializer = new XMLSerializer()
   const xmlBuffer = xmlSerializer.serializeToString(doc)
